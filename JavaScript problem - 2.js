f13b4f0d2c2f19bec2280c020a782e869e0f6424
rafi542 <mdamirhussainrafi@gmail.com>
2025-02-27 23:30:42 +0600

    Create JavaScript problem - 2.js

---
+++ JavaScript problem - 2.js
@@ -0,0 +1,4 @@
+//take an integer (last digit of your ID) as input and print the next integer of it.
+
+let idLastDigit = parseInt(prompt("Enter the last digit of your ID:"));
+console.log("Next integer:", idLastDigit + 1);
